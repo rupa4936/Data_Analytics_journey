98e3ed5f5fc6bffb1aacf159ae9f858618296f5b
Rupa <techpro6020@gmail.com>
2026-02-15 15:08:50 +0530

    Added day 1 sql file

---
+++ day1_sql_practice.sql
@@ -0,0 +1,13 @@
+-- Day 1 SQL Practice
+
+CREATE TABLE students (
+    id INT,
+    name VARCHAR(50),
+    age INT
+);
+
+INSERT INTO students VALUES (1, 'Rupa', 20);
+INSERT INTO students VALUES (2, 'Amit', 21);
+INSERT INTO students VALUES (3, 'Neha', 22);
+
+SELECT * FROM students;
